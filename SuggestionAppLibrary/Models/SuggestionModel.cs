85b1db2d9f55b1d5b412e27c62ceab018525391c
George Korolev <mr.vader1@mail.ru>
2022-04-26 22:24:45 +0300

    Добавьте файлы проекта.

---
+++ SuggestionAppLibrary/Models/SuggestionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class SuggestionModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Suggestion { get; set; }
+    public string Discription { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public CategoryModel Category { get; set; }
+    public StatusModel SuggestionStatus { get; set; }
+    public BasicUserModel Author { get; set; }
+    public HashSet<string> UserVotes { get; set; } = new();
+    public string OwnerNotes { get; set; }
+    public bool ApprovedForRelease { get; set; }
+    public bool Archived { get; set; }
+    public bool Rejected { get; set; }
+}
